e8d5192c1250cecd19b4042d3fd20b328dcd9b8c
Tricky <riosandres294@gmail.com>
2025-07-19 12:46:23 -0500

    making some test

--- main.c
+++ main.c
@@ -5,5 +5,8 @@ int main(void)
 {
     // TODO open and read de FILE
 
+    // TODO insert to the hash table the character's name, number of lines and a linked to
+    // their lines
+
     return 0;
 }
