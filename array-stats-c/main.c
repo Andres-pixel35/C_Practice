21b6ea51bf6d8aa39c9b7a5133035419bdee1786
Andres Rios <riosandres294@gmail.com>
2025-08-02 16:35:43 -0500

    Update main.c

--- array-stats-c/main.c
+++ array-stats-c/main.c
@@ -6,7 +6,7 @@ int totalSum(int a[], int b);
 int highestNumber(int a[], int b);
 int lowestNumber(int a[], int b);
 int primeNumbers(int a[], int b);
-#define MAX_NUMBERS 100 // this program will only alow max 100 differents numbers
+#define MAX_NUMBERS 100 // this program will only allow max 100 differents numbers
 
 int main(void)
 {
